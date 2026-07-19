b295fd414c26468a7bcc4d8daddc099d162154ef
Johnny <alsenycamaraditcamaraalseny@gmail.com>
2026-07-19 21:28:20 +0000

    debug(sync): v9.8.1 - bump pour forcer rechargement cache SW + logs diagnostic stock

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first PWA strategy pour fonctionnement 100% offline
  */
 
-const CACHE_NAME = 'pharma-cache-v9.8.0';
+const CACHE_NAME = 'pharma-cache-v9.8.1';
 const ASSETS = [
   './',
   './index.html',
